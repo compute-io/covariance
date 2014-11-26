082963e67ebb5928514c9f31e5a07d57c4eb0116
kgryte <kgryte@gmail.com>
2014-11-26 11:59:27 -0800

    [UPDATE] incremental algorithm

--- test/test.js
+++ test/test.js
@@ -53,8 +53,47 @@ describe( 'compute-covariance', function tests() {
 		}
 	});
 
-	it( 'should compute the sample covariance' );
+	it( 'should return a zero matrix if array length is less than 2', function test() {
+		var x, y, expected, actual;
 
-	it( 'should compute the sample variance when provided an array of arrays' );
+		x = [ 1 ];
+		y = [ -1 ];
+
+		expected = [ [0,0], [0,0] ];
+		actual = cov( x, y );
+
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should compute the sample covariance', function test() {
+		var x, y, expected, actual;
+
+		x = [ 1, -1 ];
+		y = [ -1, 1 ];
+
+		expected = [ [2,-2],[-2,2] ];
+		actual = cov( x, y );
+
+		assert.deepEqual( actual, expected );
+
+		x = [ 3, -2, 5, 4 ];
+		y = [ 6, -2, 3, -1 ];
+
+		expected = [ [29/3,6], [6,41/3] ];
+		actual = cov( x, y );
+
+		assert.deepEqual( actual, expected );
+	});
+
+	it( 'should compute the sample variance when provided an array of arrays', function test() {
+		var data, expected, actual;
+
+		data = [ [1,-1], [-1,1] ];
+
+		expected = [ [2,-2],[-2,2] ];
+		actual = cov( data );
+
+		assert.deepEqual( actual, expected );
+	});
 
 });
